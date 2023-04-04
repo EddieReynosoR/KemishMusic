c9e2d6498728ccb0597d9039d5cfb7b3370ace0c
Yahirs19 <L21212055@tectijuana.edu.mx>
2023-04-04 12:30:27 -0700

    AMG

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -408,9 +408,9 @@ namespace KemishMusic
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(787, 140);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.Size = new System.Drawing.Size(59, 20);
             this.checkBox1.TabIndex = 12;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "AMG";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
